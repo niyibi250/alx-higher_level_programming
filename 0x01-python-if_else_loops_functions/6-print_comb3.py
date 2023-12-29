6c57dd817560fed7eb209dcc1176997a0f06eb84
Replit user <>
2023-12-29 10:17:04 +0000

    second project for python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 for i in range(10):
-        for j in range(1,10):
-            if i<j and i!=j:
-                if i!=8 or j!=9:
-                        print("{0:}{1:}".format(i,j), end=', ')
-                elif i==8 and j==9:
-                        print("{0:}{1:}".format(i,j))
-            else:
-                continue
+    for j in range(1, 10):
+        if i < j and i != j:
+            if i != 8 or j != 9:
+                print("{0:}{1:}".format(i, j), end=', ')
+            elif i == 8 and j == 9:
+                print("{0:}{1:}".format(i, j))
+        else:
+            continue
